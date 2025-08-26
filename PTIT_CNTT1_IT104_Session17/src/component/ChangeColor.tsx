ff1aa94c5dc6e42a1d9e03bc4e467c73991b7bba
Đăng Việt <trandangviet00@gmail.com>
2025-08-26 22:12:24 +0700

    Add files via upload

--- PTIT_CNTT1_IT104_Session17/src/component/ChangeColor.tsx
+++ PTIT_CNTT1_IT104_Session17/src/component/ChangeColor.tsx
@@ -3,15 +3,20 @@ import { useState } from "react";
 const ChangeColor = () => {
   const [color, setColor] = useState("black");
   const handleChangeColor = () => {
-    setColor("red"); 
+    setColor(prevColor => (prevColor === "black" ? "red" : "black"));
   };
 
   return (
     <div style={{ fontFamily: "sans-serif" }}>
       <p style={{ color: color }}>Tiêu đề văn bản</p>
-      <button style={{ backgroundColor: "white", border: "1px solid grey", padding: "5px", borderRadius: "4px" }} onClick={handleChangeColor}>Thay đổi màu</button>
+      <button
+        style={{backgroundColor: "white",border: "1px solid grey",padding: "5px",borderRadius: "4px"}}
+        onClick={handleChangeColor}
+      >
+        Thay đổi màu
+      </button>
     </div>
   );
-}
+};
 
 export default ChangeColor;
